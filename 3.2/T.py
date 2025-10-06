1b830eb8d9d8ad9bb734daff8203c4c1f5663d26
Mikhail <git9it@yandex.ru>
2025-10-06 12:08:51 +0300

    T resolved with nod function

--- 3.2/T.py
+++ 3.2/T.py
@@ -1,32 +1,29 @@
+def nod(a, b):
+    c = 0
+    if a < b:
+        a, b = max(b, a), min(a, b)
+    while b:        
+        res = a % b
+        if res == 0:
+            return b
+        c = b % res
+        a = res 
+        b = c
+    return res
+
+
 string1 = input().split("; ")
+set1 = set(string1)
 l1 = []
-for _ in range(len(string1)):
-    digit1 = string1[_]
-    for digit2 in string1:
-        
-        if int(digit2) >= int(digit1):
-            condition = (
-                (int(digit2) % int(digit1) == 0) or
-                (int(digit2) % 2 == 0 and int(digit1) % 2 == 0) or
-                (int(digit2) % 3 == 0 and int(digit1) % 3 == 0) or
-                (int(digit2) % 5 == 0 and int(digit1) % 5 == 0) 
-            )
-            if condition:
-                continue
-            else:
-                l1.append(digit2)
+string1 = list(set1)
+sorted_string = sorted(string1, key=int)
+for _ in range(len(sorted_string)):
+    digit1 = sorted_string[_]
+    for digit2 in sorted_string:
+        if nod(int(digit2), int(digit1)) > 1:
+            continue
         else:
-            condition = (
-                (int(digit1) % int(digit2) == 0) or
-                (int(digit2) % 2 == 0 and int(digit1) % 2 == 0) or 
-                (int(digit2) % 3 == 0 and int(digit1) % 3 == 0) or
-                (int(digit2) % 5 == 0 and int(digit1) % 5 == 0) 
-            )
-            if condition:
-                continue
-            else:
-                l1.append(digit2)
-            
+            l1.append(digit2)
     if len(l1) > 0:
         print(f"{digit1} - {', '.join(l1)}")
         l1.clear()
